c7fd04164325ae9fbe3c61dfa1f09bf55799d25b
JackEAllen <jack.allen@stfc.ac.uk>
2022-03-08 11:07:36 +0000

    Initial commit

---
+++ system_tests/lewis_emulators/B17Tmag/__init__.py
@@ -0,0 +1,5 @@
+from .device import SimulatedB17Tmag
+from ..lewis_versions import LEWIS_LATEST
+
+framework_version = LEWIS_LATEST
+__all__ = ['SimulatedB17Tmag']
---
+++ system_tests/lewis_emulators/B17Tmag/interfaces/__init__.py
@@ -0,0 +1,3 @@
+from .stream_interface import B17TmagStreamInterface
+
+__all__ = ['B17TmagStreamInterface']
